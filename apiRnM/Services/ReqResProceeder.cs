bd758175f19cc9c381592f65eda3b6e61ecdf08b
o-de-code <oleksandr.derevianchuk@gmail.com>
2023-05-12 15:02:38 +0300

    small fix "Not Found"

--- apiRnM/Services/ReqResProceeder.cs
+++ apiRnM/Services/ReqResProceeder.cs
@@ -18,16 +18,17 @@ namespace apiRnM.Services
         public bool CheckPersonInEpisode(string personName, string episodeName, out bool found)
         {
             var personList = _apiCaller.GetPersons(personName);
-            var episode = _apiCaller.GetEpisodes(episodeName).First();
+            var episodeList = _apiCaller.GetEpisodes(episodeName);
             found = true;
 
-            if(!personList.Any() || episode == null)
+            if(!personList.Any() || episodeList == null)
             {
                 found = false;
                 return false;
             }
             else
             {
+                var episode = episodeList.First();
                 var allEpisodesQuery = personList.Select(
                     person => person.episode.Select(
                         episodeUrl => episodeUrl.Split('/').Last()))
@@ -44,35 +45,40 @@ namespace apiRnM.Services
 
         public string GetPersonData(string PersonName)
         {
-            var persons = _apiCaller.GetPersons(PersonName).First();
-
-            List<PersonResponse> personResponse = new List<PersonResponse>();
-
-            var origin = persons.origin.ToString();
-            var jelement = JsonDocument.Parse(origin).RootElement;
-            var location = _apiCaller.GetDataFromApi(jelement.GetProperty("url").ToString());
-            var parsedLocation = new Location();
-
-            //TO EDIT encoding
-            if (location != null)
-                parsedLocation = JsonSerializer.Deserialize<Location>(location);
-
-            personResponse.Add(new PersonResponse()
+            var persons = _apiCaller.GetPersons(PersonName);
+            string response = null;
+            if(persons.Any())
             {
-                name = persons.name,
-                gender = persons.gender,
-                species = persons.species,
-                status = persons.status,
-                type = persons.type,
-                origin = new Origin
-                {
-                    name = parsedLocation.name,
-                    dimension = parsedLocation.dimension,
-                    type = parsedLocation.type
-                }
-            });
+                List<PersonResponse> personResponse = new List<PersonResponse>();
+                var person = persons.First();
+                var origin = person.origin.ToString();
+                var jelement = JsonDocument.Parse(origin).RootElement;
+                var location = _apiCaller.GetDataFromApi(jelement.GetProperty("url").ToString());
+                var parsedLocation = new Location();
 
-            var response = JsonSerializer.Serialize(personResponse);
+                //TO EDIT encoding
+                if (location != null)
+                    parsedLocation = JsonSerializer.Deserialize<Location>(location);
+
+                personResponse.Add(new PersonResponse()
+                {
+                    name = person.name,
+                    gender = person.gender,
+                    species = person.species,
+                    status = person.status,
+                    type = person.type,
+                    origin = new Origin
+                    {
+                        name = parsedLocation.name,
+                        dimension = parsedLocation.dimension,
+                        type = parsedLocation.type
+                    }
+                });
+
+                response = JsonSerializer.Serialize(personResponse);
+            }
+
+            
             return response;
         }
     }
